3b1cd0b91db121b90bdee8149dd99d636a05f819
lixxiang <1548806494@qq.com>
2018-02-28 15:17:02 +0800

    1。

--- emall-ec/src/main/java/com/example/emall_ec/main/detail/VideoDetailDataConverter.kt
+++ emall-ec/src/main/java/com/example/emall_ec/main/detail/VideoDetailDataConverter.kt
@@ -1,12 +1,23 @@
 package com.example.emall_ec.main.detail
 
+import com.alibaba.fastjson.JSON
+import com.example.emall_core.ui.recycler.MultipleFields
 import com.example.emall_core.ui.recycler.MultipleItemEntity
+import com.example.emall_core.util.log.EmallLogger
+import com.example.emall_ec.main.index.VideoDetailFields
 
 /**
  * Created by lixiang on 2018/2/28.
  */
 class VideoDetailDataConverter : DetailDataConverter(){
     override fun convert(): ArrayList<MultipleItemEntity> {
+        val dataArray = JSON.parseObject(getJsonData()).getJSONArray("data")
+        EmallLogger.d(dataArray)
+//        val entity = MultipleItemEntity.builder()
+//                        .setField(VideoDetailFields.CLOUD, )
+//                        .setField(VideoDetailFields.SPAN_SIZE, 2)
+//                        .setField(VideoDetailFields.ITEM_TYPE, 2)
+//                        .build()
     }
 
 
---
+++ emall-ec/src/main/java/com/example/emall_ec/main/index/VideoDetailFields.kt
@@ -0,0 +1,9 @@
+package com.example.emall_ec.main.index
+
+/**
+ * Created by lixiang on 2018/2/28.
+ */
+enum class VideoDetailFields {
+    CLOUD,
+    DURATION
+}
