3fd195f7969560c7e6222660f955947ca2294b50
lixxiang <1548806494@qq.com>
2018-03-01 15:49:15 +0800

    1。can post

--- emall-ec/src/main/java/com/example/emall_ec/main/order/OrderDelegate.kt
+++ emall-ec/src/main/java/com/example/emall_ec/main/order/OrderDelegate.kt
@@ -4,6 +4,7 @@ import com.bumptech.glide.Glide
 import com.example.emall_core.delegates.bottom.BottomItemDelegate
 import com.example.emall_core.net.RestClient
 import com.example.emall_core.net.RestCreator
+import com.example.emall_core.net.RestCreator.params
 import com.example.emall_core.net.callback.IError
 import com.example.emall_core.net.callback.IFailure
 import com.example.emall_core.net.callback.ISuccess
@@ -19,12 +20,15 @@ import java.util.*
 /**
  * Created by lixiang on 2018/3/1.
  */
-class OrderDelegate : BottomItemDelegate(){
+class OrderDelegate : BottomItemDelegate() {
 
-    var params :WeakHashMap<String, Any> ?= WeakHashMap()
+    var commoditySubmitDemandParams: WeakHashMap<String, Any>? = WeakHashMap()
+    var viewDemandParams: WeakHashMap<String, Any>? = WeakHashMap()
+
     fun create(): OrderDelegate? {
         return OrderDelegate()
     }
+
     override fun setLayout(): Any? {
         return R.layout.delegate_order
     }
@@ -36,21 +40,46 @@ class OrderDelegate : BottomItemDelegate(){
             "http://10.10.90.38:3033/data"
         }
 
-        params!!["productId"]= "JL101A_PMS_20170725103441_000020182_102_0025_002_L1_PAN;JL101A_PMS_20170725103441_000020182_102_0029_002_L1_PAN"
-        params!!["status"] = "1"
-        params!!["type"] = "1"
-        params!!["geo"] = "0"
+        commoditySubmitDemandParams!!["productId"] = "JL101A_PMS_20170725103441_000020182_102_0025_002_L1_PAN;JL101A_PMS_20170725103441_000020182_102_0029_002_L1_PAN"
+        commoditySubmitDemandParams!!["status"] = "1"
+        commoditySubmitDemandParams!!["type"] = "1"
+        commoditySubmitDemandParams!!["geo"] = "0"
 
-        EmallLogger.d(params!!)
+        EmallLogger.d(commoditySubmitDemandParams!!)
         RestClient().builder()
                 .url("http://10.10.90.11:8086/global/commoditySubmitDemand")//EMULATOR
-                .params(params!!)
+                .params(commoditySubmitDemandParams!!)
                 .success(object : ISuccess {
                     override fun onSuccess(response: String) {
                         EmallLogger.d(response)
 //                        val bannerSize = VideoDetailDataConverter().setJsonData(response).convert()
 //                        EmallLogger.d(bannerSize[0].getField(VideoDetailFields.DURATION))
 //                        DATA = bannerSize
+                        viewDemandParams!!["type"] = "1"
+                        viewDemandParams!!["demandId"] = "180301143300028664"
+                        RestClient().builder()
+                                .url("http://10.10.90.11:8086/global/viewDemand")//EMULATOR
+                                .params(viewDemandParams!!)
+                                .success(object : ISuccess {
+                                    override fun onSuccess(response: String) {
+                                        EmallLogger.d(response)
+//                        val bannerSize = VideoDetailDataConverter().setJsonData(response).convert()
+//                        EmallLogger.d(bannerSize[0].getField(VideoDetailFields.DURATION))
+//                        DATA = bannerSize
+                                    }
+                                })
+                                .failure(object : IFailure {
+                                    override fun onFailure() {
+
+                                    }
+                                })
+                                .error(object : IError {
+                                    override fun onError(code: Int, msg: String) {
+
+                                    }
+                                })
+                                .build()
+                                .post()
                     }
                 })
                 .failure(object : IFailure {
